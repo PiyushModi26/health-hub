edb9e0e6f018c0987616588beb3a628680b46174
Dax patel <daxpatel2701@gmail.com>
2025-08-07 11:02:37 +0530

    sign up sign in improve

--- src/pages/AuthPage.jsx
+++ src/pages/AuthPage.jsx
@@ -34,12 +34,12 @@ const AuthPage = () => {
             <div className="overlay-panel overlay-left">
               <h1>Welcome Back!</h1>
               <p>To keep connected with us please login with your personal info</p>
-              <button onClick={() => setIsLoginView(true)}>Sign In</button>
+              <button onClick={() => setIsLoginView(true)}>Sign Up</button>
             </div>
             <div className="overlay-panel overlay-right">
               <h1>Hello, Friend!</h1>
               <p>Enter your personal details and start your journey with us</p>
-              <button onClick={() => setIsLoginView(false)}>Sign Up</button>
+              <button onClick={() => setIsLoginView(false)}>Sign In</button>
             </div>
           </div>
         </div>
